8a7be084a40b7aa3507baf58f34cd1774dce24a4
Guy <guy-sha@github.com>
2022-05-21 13:55:59 +0000

    probably implemented get/set_status syscalls, and set the ground for everything else

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -941,5 +941,9 @@ asmlinkage long sys_pkey_free(int pkey);
 asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
 asmlinkage long sys_hello(void);
+asmlinkage long sys_set_status(int status);
+asmlinkage long sys_get_status(void);
+asmlinkage long sys_register_process(void);
+asmlinkage long sys_get_all_cs(void);
 
 #endif
--- kernel/hw2.c
+++ kernel/hw2.c
@@ -1,6 +1,29 @@
 #include <linux/kernel.h>
+#include <linux/list.h>
+#include <linux/sched.h>
 
 asmlinkage long sys_hello(void) {
 	printk("Hello, World!\n");
+	return 0;
+}
+
+asmlinkage long sys_set_status(int status) {
+	if (status != 0 && status != 1) {
+		return -EINVAL;
+	}
+	
+	current->status = status;
+	return 0;
+}
+
+asmlinkage long sys_get_status(void) {
+	return current->status;
+}
+
+asmlinkage long sys_register_process(void) {
+	return 0;
+}
+
+asmlinkage long sys_get_all_cs(void) {
 	return 0;
 }
